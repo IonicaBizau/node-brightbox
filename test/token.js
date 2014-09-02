d432879bf40ba433d9cc3186e7aff18eaa6df494
Ionică Bizău <bizauionica@yahoo.com>
2014-09-02 09:18:48 +0300

    Set auth once

--- test/token.js
+++ test/token.js
@@ -1,9 +1,8 @@
 var Brightbox = require("../lib/brightboxapis");
 var Credentials = require("./credentials");
 var Auth = new Brightbox.auth(Credentials.clientId, Credentials.clientSecret);
+Brightbox.options({auth: Auth})
 
-Brightbox.servers.list({
-    auth: Auth
-}, function (err, servers) {
+Brightbox.servers.list({}, function (err, servers) {
     console.log(err || "You have " + servers.length + " cloud servers.");
 });
