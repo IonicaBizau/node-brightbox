5e07398c1b00d0f34ffea65992c666aa7afbace1
Ionică Bizău <bizauionica@yahoo.com>
2015-02-03 17:02:56 +0200

    Updated test

--- test/index.js
+++ test/index.js
@@ -13,9 +13,9 @@ Brightbox.users.list({}, function (err, users) {
     console.log("Fetching " + users[0].name);
     Brightbox.users.get({id: users[0].id}, function (err, user) {
         console.log(user);
+        console.log("Fetching the server types.");
+        Brightbox.server_types.list({}, function (err, server_types) {
+            console.log(server_types);
+        });
     });
-});
-
-Brightbox.server_types.list({}, function (err, server_types) {
-    console.log(server_types);
 });
